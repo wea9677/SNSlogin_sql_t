f674bb4f07e3caf800d328e28aff702f2fc34285
wea9677 <wea9677@naver.com>
2022-07-02 01:48:42 +0900

    콘솔때문이지?

--- passport/kakaoStrategy.js
+++ passport/kakaoStrategy.js
@@ -17,7 +17,7 @@ module.exports = () => {
              * profile: 카카오가 보내준 유저 정보. profile의 정보를 바탕으로 회원가입
              */
             async (accessToken, refreshToken, profile, done) => {
-                // console.log('kakao profile', profile)
+                 console.log('kakao profile', profile)
                 try {
                     console.log("지나가라 ㅠㅠ");
                     const userInfo = await Users.findAll({})
